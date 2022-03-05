fb5c9204aa302a0c454910a7af45835774b68d5b
Becca-Chae <dabin.chae55@gmail.com>
2022-03-05 21:15:14 +0900

    JS: Navbar menu toggle + Project UI tweak

--- main.js
+++ main.js
@@ -12,6 +12,8 @@ document.addEventListener('scroll', ()=> {
     }
 });
 
+
+
 //Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event)=> {
@@ -20,9 +22,17 @@ navbarMenu.addEventListener('click', (event)=> {
   if (link == null) {
     return;
   }
+  navbarMenu.classList.remove('open')
 
   scrollIntoView(link);
-})
+});
+
+// Navbar toggle button form small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', ()=> {
+  navbarMenu.classList.toggle('open');
+});
+
 
 // Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
